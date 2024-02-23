c16a0ff7cc62b719208de32100d6a4aa3cbd1c72
MeetB7 <meetbrijwani@gmail.com>
2024-02-23 22:38:02 +0530

    onli pdf result remain

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import webbrowser
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 
-global num_variables, num_constraints, constraints_entries, objective_entries, tableau, tableau_list, res_file
+global z_value, result, num_variables, num_constraints, constraints_entries, objective_entries, tableau, tableau_list,objtype,  res_file
 res_file = "lpp.pdf"
 
 def simplex_iteration(tableau):
@@ -39,7 +39,8 @@ def save_tableaux_to_pdf(tableau_list, filename):
         row_height = 20
         for i, row in enumerate(tableau):
             for j, val in enumerate(row):
-                c.drawString(85 + j * 50, yaxis - i * row_height, str(val))
+                formatted_val = "{:.3f}".format(val)
+                c.drawString(85 + j * 50, yaxis - i * row_height, formatted_val)
         yaxis  -= 1.5 * inch
         
     c.save()
@@ -65,6 +66,15 @@ def make_tableau(table):
     return tableau
 
 def calculate():  
+    
+    for widget in output_frame.winfo_children():
+        widget.destroy()
+        
+    global objtype
+    if(obj_var.get() == 'Maximize'):
+        objtype = 0
+    else: objtype = 1 
+
     table = np.zeros((num_constraints+1, num_variables+1),dtype=float)
 
     for i in range(num_constraints):
@@ -77,6 +87,8 @@ def calculate():
         table[-1][i] = entry_value
         
     global tableau
+    if (objtype == 1):
+        table = table.transpose()
     tableau = make_tableau(table) # implementing slack variables is remaining
     print(tableau)
     
@@ -92,10 +104,35 @@ def calculate():
     # for tableau in tableau_list:
     #     print(tableau,end="\n")
     # Save all tableaux to PDF
+    
+    
+    ttk.Label(output_frame, text="Result: ",font=custom_font).grid(row=3,column=0,pady=5)
+    global result
+    result = []
+    global z_value
+    z_value = "{:0.3f}".format(tableau[-1][-1])
+    if(objtype == 0):
+        for i in range(0,num_variables):
+            result.append("{:0.3f}".format(tableau[i][-1]))
+    else: 
+        for i in range(num_variables,num_variables+num_variables):
+            result.append("{:0.3f}".format(tableau[-1][i]))
+    
+    ttk.Label(output_frame,text=f"Z = {z_value}" , font=custom_font).grid(row=4, column=0, padx=3, pady=5)
+    if(objtype == 0):
+        ttk.Label(output_frame,text="Z is maximum at: ",font=custom_font).grid(row=5,column=0,pady=5,padx=3)
+    else: ttk.Label(output_frame,text="Z is minimum at: ",font=custom_font).grid(row=5,column=0,pady=5,padx=3)
+    cur_row = 5
+    for i in range(1, num_variables+1):
+        ttk.Label(output_frame,text=f"x{i} = {result[i-1]}",font=custom_font).grid(row=cur_row+1,column=0,padx=3,pady=5)
+        cur_row += 1
+        
     save_tableaux_to_pdf(tableau_list, res_file)
     
-    pdf_button = ttk.Button(root, text="PDF", command=lambda: webbrowser.open_new(res_file) , width=15)
-    pdf_button.grid(row=10, column=1, pady=10) # pos to be changed 
+    ttk.Label(output_frame,text="For detailed solution with each iteration of tableau => ",font=custom_font).grid(row=10,column=0,padx=3,pady=5)
+    pdf_button = ttk.Button(output_frame, text="PDF", command=lambda: webbrowser.open_new(res_file) , width=15)
+    pdf_button.grid(row=10, column=1, pady=10)
+    
     
 def generate():
     global num_variables,num_constraints
@@ -139,8 +176,14 @@ def generate():
         entries_row.append(rhs_entry)
         constraints_entries.append(entries_row)
     
-    calculate_button = ttk.Button(root, text="Calculate", command=calculate, width=15)
-    calculate_button.grid(row=6, column=1, pady=10)
+    calculate_button = ttk.Button(output_frame, text="Calculate", command=calculate, width=15)
+    calculate_button.grid(row=1, column=2, pady=10)
+    
+    global obj_var
+    obj_var = tk.StringVar()
+    objective_type = ttk.Combobox(output_frame, textvariable=obj_var, values=obj_types)
+    objective_type.grid(row= 1,column=0, pady=10)
+    objective_type.current(0)
 
 def hasNegativeEntry(tableau):
     for entry in tableau[-1]:
@@ -151,6 +194,7 @@ def hasNegativeEntry(tableau):
 # Options for the dropdowns
 var_options = [2, 3, 4, 5, 6, 7, 8, 9]
 eq_options = ['<=', '>=']
+obj_types = ['Maximize' , 'Minimize']
 
 # Create the main window
 root = tk.Tk()
@@ -178,6 +222,9 @@ generate_button.grid(row=2, column=1, pady=10)
 
 # Create the input frame
 input_frame = ttk.Frame(root)
-input_frame.grid(row=3, column=0, columnspan=2)
+input_frame.grid(row=3, column=0, columnspan=3)
+
+output_frame = ttk.Frame(root)
+output_frame.grid(row=4, column=0, columnspan=4)
 
 root.mainloop()
